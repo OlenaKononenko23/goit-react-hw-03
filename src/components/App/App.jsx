8b37e892cda70088037eb8c5ec356c80215c0129
OlenaKononenko23 <148655722+OlenaKononenko23@users.noreply.github.com>
2024-05-08 08:47:41 +0300

    projectAll

---
+++ src/components/App/App.jsx
@@ -0,0 +1,17 @@
+
+import css from './App.module.css';
+
+export default function App() {
+  
+
+  return (
+    <>
+      <h1>Phonebook</h1>
+      {/* <ContactForm />
+      <SearchBox />
+      <ContactList /> */}
+    </>
+  )
+}
+
+
